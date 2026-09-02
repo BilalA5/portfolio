33ee6668d0647cccadef15b017696213923f2c9d
bahmed721 <s.bilal.ahmed927@gmail.com>
2026-09-01 21:00:17 -0600

    feat: publish the Open Graph identity card

---
+++ portfolio/app/opengraph-image.tsx
@@ -0,0 +1,14 @@
+import {
+  renderSocialImage,
+  socialImageAlt,
+  socialImageContentType,
+  socialImageSize,
+} from "./social-card";
+
+export const alt = socialImageAlt;
+export const size = socialImageSize;
+export const contentType = socialImageContentType;
+
+export default function OpenGraphImage() {
+  return renderSocialImage();
+}
